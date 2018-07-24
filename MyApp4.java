858594b96760cfaa0977326af7ce5cda80e5541e
Sho Imai <aplieomavacere@gmail.com>
2018-07-24 13:23:11 +0900

    演算

---
+++ MyApp4.java
@@ -0,0 +1,27 @@
+  public class MyApp4 {
+    public static void main(String[] args) {
+      //演算
+      //+ - * / %
+      //++ --
+      int i;
+      i = 10/3;
+      System.out.println(i); //小数点切捨てで3
+
+      i = 10%3;
+      System.out.println(i);
+
+      int x = 5;
+      x++;
+      System.out.println(x);
+      x--;
+      System.out.println(x);
+
+      x += 12;
+      System.out.println(x); //17
+
+      String s;
+      s = "hello" + " world";
+      System.out.println(s);
+
+    }
+  }
