b83c7a8513977bbd8398c7f164fc7eae1091dbe6
Sho Imai <aplieomavacere@gmail.com>
2018-07-24 14:03:39 +0900

    型変換

---
+++ MyApp5.java
@@ -0,0 +1,12 @@
+  public class MyApp5 {
+    public static void main(String[] args) {
+      //型変換(キャスト)
+      double d =52343.231;
+      int i = (int)d;
+      System.out.println(i);
+
+      i = 10;
+      d = (double)i/4;
+      System.out.println(d);
+    }
+  }
